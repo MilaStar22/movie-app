943edebb3e7eb6fe7670b3cbf1653572d98812ba
Mila_Star <101258091+MilaStar22@users.noreply.github.com>
2023-07-05 17:41:54 +0200

    remove comment code from  Actors component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Router basename="/movie-app">
+  <Router basename="/movie-app"> 
     <App />
   </Router>
   </React.StrictMode>
